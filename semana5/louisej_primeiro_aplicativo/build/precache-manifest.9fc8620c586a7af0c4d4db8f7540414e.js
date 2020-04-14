fe6514bd4c9d504b0b94465c380d108d297152ee
LouiseJaqdev <louisejaqdev@gmail.com>
2020-04-14 13:59:58 -0300

    alteração

---
+++ semana5/louisej_primeiro_aplicativo/build/precache-manifest.9fc8620c586a7af0c4d4db8f7540414e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c82b137b1d7e33c5e58d136b7c059d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640e80b23951a7948e46",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69e2cd5f56e5c43da6f6",
+    "url": "/static/js/2.fc3bac2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc3bac2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640e80b23951a7948e46",
+    "url": "/static/js/main.d812d84c.chunk.js"
+  },
+  {
+    "revision": "d6ddbe982d3d1683080c",
+    "url": "/static/js/runtime-main.43fa71a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
