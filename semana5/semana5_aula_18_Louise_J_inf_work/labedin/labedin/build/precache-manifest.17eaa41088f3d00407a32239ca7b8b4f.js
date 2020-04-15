8e714fb70c989265e1c5689db97b2ec57aecf43c
LouiseJaqdev <louisejaqdev@gmail.com>
2020-04-15 15:10:30 -0300

    segundo commit
    parcial 2

---
+++ semana5/semana5_aula_18_Louise_J_inf_work/labedin/labedin/build/precache-manifest.17eaa41088f3d00407a32239ca7b8b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0e135b9415f4798cf0bf895001aa75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531d1c522cf84da60f94",
+    "url": "/static/css/main.cb4ecaf2.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531d1c522cf84da60f94",
+    "url": "/static/js/main.d3d43447.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);
