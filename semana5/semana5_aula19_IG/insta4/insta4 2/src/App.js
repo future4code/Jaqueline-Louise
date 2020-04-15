e08c3dfcbd600e9accf1f9ed4516c53a44cc25eb
LouiseJaqdev <louisejaqdev@gmail.com>
2020-04-15 17:34:15 -0300

    commit 3
    ainda estou fazendo, não consegui linkar a imagem, e só terei tempo para rever amanhã porque agora estou indo para o trabalho

--- semana5/semana5_aula19_IG/insta4/insta4 2/src/App.js
+++ semana5/semana5_aula19_IG/insta4/insta4 2/src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
 
+
+
 class App extends React.Component {
-  render() {
-    return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = { 'app-container' } >
+            <
+            Post nomeUsuario = { 'LouiseJ' }
+            fotoUsuario = {''}
+            fotoPost = { 'https://picsum.photos/200/150' }
+            /> < /div >
+        );
+    }
 }
+
+
+   
+
 
 export default App;
